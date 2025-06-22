8b16640dd7bc6be46ca250c11e15272579682079
martin <goudingcheng@gmail.com>
2025-06-22 19:52:59 +0800

    add custom event

---
+++ jquick-pdfx/src/main/java/com/github/paohaijiao/event/JPdfXPageEventHandler.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) [2025-2099] Martin (goudingcheng@gmail.com)
+ */
+package com.github.paohaijiao.event;
+
+import com.itextpdf.kernel.events.Event;
+import com.itextpdf.kernel.events.IEventHandler;
+import com.itextpdf.kernel.events.PdfDocumentEvent;
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+/**
+ * packageName com.github.paohaijiao.event
+ *
+ * @author Martin
+ * @version 1.0.0
+ * @className JPdfXPageEventHandler
+ * @date 2025/6/22
+ * @description
+ */
+public class JPdfXPageEventHandler implements IEventHandler {
+    @Override
+    public void handleEvent(Event event) {
+        PdfDocumentEvent docEvent = (PdfDocumentEvent) event;
+        PdfDocument pdfDoc = docEvent.getDocument();
+        PdfPage page = docEvent.getPage();
+        Rectangle pageSize = page.getPageSize();
+        // 获取Canvas对象进行绘制
+        PdfCanvas pdfCanvas = new PdfCanvas(page.newContentStreamBefore(), page.getResources(), pdfDoc);
+        pdfCanvas.beginText()
+                .setFontAndSize(pdfDoc.getDefaultFont(), 10)
+                .moveText(pageSize.getWidth() / 2 - 30, pageSize.getHeight() - 20)
+                .showText("Page Header")
+                .endText();
+        pdfCanvas.release();
+    }
+}
---
+++ jquick-pdfx/src/main/java/com/github/paohaijiao/event/JPdfXWatermarkEventHandler.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (c) [2025-2099] Martin (goudingcheng@gmail.com)
+ */
+package com.github.paohaijiao.event;
+
+import com.itextpdf.io.font.constants.StandardFonts;
+import com.itextpdf.kernel.colors.ColorConstants;
+import com.itextpdf.kernel.events.Event;
+import com.itextpdf.kernel.events.IEventHandler;
+import com.itextpdf.kernel.events.PdfDocumentEvent;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.geom.AffineTransform;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvasConstants;
+
+import java.io.IOException;
+
+import static com.itextpdf.kernel.pdf.PdfName.FontFamily;
+
+/**
+ * packageName com.github.paohaijiao.event
+ *
+ * @author Martin
+ * @version 1.0.0
+ * @className JPdfXWatermarkEventHandler
+ * @date 2025/6/22
+ * @description
+ */
+public class JPdfXWatermarkEventHandler implements IEventHandler {
+    @Override
+    public void handleEvent(Event event) {
+        PdfDocumentEvent docEvent = (PdfDocumentEvent) event;
+        PdfPage page = docEvent.getPage();
+        PdfFont font =null;
+        try {
+             font = PdfFontFactory.createFont(StandardFonts.HELVETICA);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        PdfCanvas canvas = new PdfCanvas(page.newContentStreamBefore(), page.getResources(), docEvent.getDocument());
+        canvas.saveState()
+                .setFillColor(ColorConstants.LIGHT_GRAY)
+                .setTextMatrix(30, 30)
+                .beginText()
+                .setFontAndSize(font,60)
+                .setTextRenderingMode(PdfCanvasConstants.TextRenderingMode.FILL)
+                .setTextMatrix(AffineTransform.getRotateInstance(Math.PI / 4))
+                .showText("DRAFT")
+                .endText()
+                .restoreState();
+
+        canvas.release();
+    }
+}
